05fd06b28be4b721a5a22896836f9e46c875237d
MohdFazil45 <mdfazil846@gmail.com>
2025-09-26 21:46:32 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="text-white">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/agence" element={<Agence/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </div>
   )
 }
 
